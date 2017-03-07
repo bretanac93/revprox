31606aac7635ea9c257cdc4bbb4b277114034b5c
Cesar Bretana Gonzalez <bretanac@gmail.com>
2017-03-07 11:18:55 -0500

    CodeMirror added and configured

--- resources/views/admin/files/edit.blade.php
+++ resources/views/admin/files/edit.blade.php
@@ -2,6 +2,14 @@
 @section('title', 'Editar Proxy')
 @section('page_header', 'Editar Proxy')
 
+@section('page_level_styles')
+    <link rel="stylesheet" href="{{ asset('plugins/codemirror/lib/codemirror.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/codemirror/lib/material.css') }}">
+
+    <script src="{{ asset('plugins/codemirror/lib/codemirror.js') }}"></script>
+    <script src="{{ asset('plugins/codemirror/lib/nginx.js') }}"></script>
+
+@stop
 @section('content')
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -9,11 +17,28 @@
             <form method="POST" id="form" action="{{ url(route('files.update', ['id' => $id])) }}" accept-charset="UTF-8">
                 <input type="hidden" name="_method" value="PUT">
                 {{ csrf_field() }}
-                <textarea name="file_content" id="file_content" cols="30">
-                    {{ $content }}
-                </textarea>
+                <div class="form-group">
+                    <textarea name="file_content" id="file_content" cols="30">{{ $content }}</textarea>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-default" value="Editar">
+                </div>
             </form>
             <!-- /.box -->
         </div>
     </div>
+@stop
+
+@section('level_scripts')
+    <script>
+        var editor = CodeMirror.fromTextArea(document.getElementById('file_content'), {
+            height: "300px",
+            lineNumbers: true,
+            styleActiveLine: true,
+            matchBrackets: true,
+            theme: 'material',
+            indentUnit: 4,
+            
+        });
+    </script>
 @stop
