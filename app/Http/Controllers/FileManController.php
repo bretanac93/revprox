932d836da49b6d5652c03e6a81b1b1c699251855
Cesar Bretana Gonzalez <bretanac@gmail.com>
2017-04-12 10:28:21 -0400

    Fixing bug related to file edition

--- app/Http/Controllers/FileManController.php
+++ app/Http/Controllers/FileManController.php
@@ -23,8 +23,8 @@ class FileManController extends Controller
 
     public function update($id) {
         $content = request('file_content');
-        $filename = ReverseProxy::find($id);
-        $func_res = NginxFacade::processFileData($content);
+        $filename = ReverseProxy::find($id)->proxy_dns;
+        $func_res = NginxFacade::processFileData($filename, $content);
         $data = $func_res[0];
         $res = $func_res[1];
 
