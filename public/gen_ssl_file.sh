3db73483368fed4e1a575bff0d45bcd004ff795b
Cesar Bretana Gonzalez <bretanac@gmail.com>
2017-05-13 09:44:32 -0400

    Migrated to letsencrypt

--- public/gen_ssl_file.sh
+++ public/gen_ssl_file.sh
@@ -6,19 +6,21 @@
 #TODO: Replace openssl for letsencrypt
 
 
-mkdir /etc/nginx/ssl 2>/dev/null
+# mkdir /etc/nginx/ssl 2>/dev/null
 
-PATH_SSL="/etc/nginx/ssl"
-PATH_KEY="${PATH_SSL}/${1}.key"
-PATH_CSR="${PATH_SSL}/${1}.csr"
-PATH_CRT="${PATH_SSL}/${1}.crt"
+# PATH_SSL="/etc/nginx/ssl"
+# PATH_KEY="${PATH_SSL}/${1}.key"
+# PATH_CSR="${PATH_SSL}/${1}.csr"
+# PATH_CRT="${PATH_SSL}/${1}.crt"
 
-if [ ! -f $PATH_KEY ] || [ ! -f $PATH_CSR ] || [ ! -f $PATH_CRT ]
-then
-  openssl genrsa -out "$PATH_KEY" 2048 2>/dev/null
-  openssl req -new -key "$PATH_KEY" -out "$PATH_CSR" -subj "/CN=$1/O=UPR/C=CU" 2>/dev/null
-  openssl x509 -req -days 365 -in "$PATH_CSR" -signkey "$PATH_KEY" -out "$PATH_CRT" 2>/dev/null
-fi
+# if [ ! -f $PATH_KEY ] || [ ! -f $PATH_CSR ] || [ ! -f $PATH_CRT ]
+# then
+#   openssl genrsa -out "$PATH_KEY" 2048 2>/dev/null
+#   openssl req -new -key "$PATH_KEY" -out "$PATH_CSR" -subj "/CN=$1/O=UPR/C=CU" 2>/dev/null
+#   openssl x509 -req -days 365 -in "$PATH_CSR" -signkey "$PATH_KEY" -out "$PATH_CRT" 2>/dev/null
+# fi
+
+certbot certonly --standalone -d $1
 
 block="server {
 
@@ -32,9 +34,8 @@ block="server {
     }
 
     error_log  /var/log/nginx/$1-error.log error;
-
-    ssl_certificate     /etc/nginx/ssl/$1.crt;
-    ssl_certificate_key /etc/nginx/ssl/$1.key;
+    ssl_certificate     /etc/letsencrypt/live/$1/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$1/privkey.pem;
 }
 "
 
